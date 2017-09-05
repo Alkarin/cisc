2deca18294eff57437b0d9209f44a9e1ab0f5a54
AlexanderV <avaughan@barefootsolutions.com>
2017-09-05 14:31:26 -0700

    Initialise from command line arguments

--- project-01/main.c
+++ project-01/main.c
@@ -45,6 +45,11 @@ struct geoLocation{
 
 int main(int argc, char* argv[]){
 
+    double  lat1;
+    double  long1;
+    double  lat2;
+    double  long2;
+
     #define PI 3.14159265
 
     // get points
@@ -53,17 +58,27 @@ int main(int argc, char* argv[]){
     for (int i = 0; i < argc; i++){
         // maybe do i+1 to avoid program arg
         printf("command line was %s\n", argv[i]);
-
+        if(i == 0){
+            // skip arg
+        } else if(i == 1){
+            sscanf(argv[i],"%lf", &lat1);
+        } else if(i == 2) {
+            sscanf(argv[i],"%lf", &long1);
+        } else if(i == 3) {
+            sscanf(argv[i],"%lf", &lat2);
+        } else if(i == 4) {
+            sscanf(argv[i],"%lf", &long2);
+        }
     }
 
     //print argc
     //int argc is number of arguments on commandline (a 123 123 = 3 args)
     printf("argc: %d \n", argc);
 
-    double  lat1 = -99.9;
-    double  long1 = 99.8;
-    double  lat2 = 99.7;
-    double  long2 = -99.6;
+    //double  lat1 = -99.9;
+    //double  long1 = 99.8;
+    //double  lat2 = 99.7;
+    //double  long2 = -99.6;
 
     struct geoLocation point1;
     struct geoLocation point2;
@@ -90,17 +105,16 @@ double distanceBetween(double lat1, double long1, double lat2, double long2){
     lat2 = toRadians(lat2);
     long2 = toRadians(long2);
 
+    // calculate distance points
     double distance = calculateDistance(lat1,long1,lat2,long2);
 
     return distance;
 }
 
 
-
 double toRadians(double degree){
 
-    // pi radians = 180 degrees
-    // multiply angle in degrees by pi/180
+    // multiply angle in degrees by pi/180 to get radians
     // ex:90 degrees
     // 90 * PI/180 = PI/2 ~ 1.57096
     printf("%lf in radians is %lf \n", degree, degree * (M_PI/180.00) );
@@ -115,14 +129,17 @@ double calculateDistance(double lat1, double long1, double lat2, double long2){
     // radius of earth in kilometers
     double RadiusKm = 6371;
 
+    // midpoint formula
     double dlong = (long2 - long1);
     double dlat = (lat2 - lat1);
 
+    // calculate distance formula
     double a = sin(dlat/2) * sin(dlat/2) + cos(lat1) * cos(lat2) * sin(dlong/2) * sin(dlong/2);
     double c = 2 * atan2(sqrt(a), sqrt(1-a));
 
+    // result
     double result = RadiusKm * c;
-    //result = kilometerToYards(result);
+    // result = kilometerToYards(result);
     return result;
 }
 
@@ -130,6 +147,7 @@ double kilometerToYards(double kilometers){
     // convert kilometers to yards
     return kilometers * 1093.6;
 }
+
 
 
 
