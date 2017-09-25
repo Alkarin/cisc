3c8f66532c8eaae177ac4c88a298e6181647f058
AlexanderV <avaughan@barefootsolutions.com>
2017-09-25 16:16:30 -0700

    Project 2 init

---
+++ project-02/main.c
@@ -0,0 +1,217 @@
+// Alexander Vaughan, av1045643
+// av1045643@swccd.edu
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+/*
+############# PROJECT INFO #############
+cplusplus.com
+C library reference, has list of all includes
+
+Expected commandline input
+C:\getDist coordinateFile.txt
+
+Calculation checked against 'haversine' formula
+http://www.movable-type.co.uk/scripts/latlong.html
+
+Various links
+// https://swccd.instructure.com/courses/6682/assignments/31434
+
+// http://www.cplusplus.com/reference/cmath/cos/
+*/
+
+// struct declarations
+struct geoLocation{
+    double latitude;
+    double longitude;
+};
+
+typedef struct latLong* link;
+
+struct latLong {
+  double lat;
+  double lng;
+  link next;
+};
+
+
+
+// method declarations
+double calculateDistance(double lat1, double long1, double lat2, double long2);
+void printResult(struct geoLocation, struct geoLocation, double result);
+double distanceBetween(struct geoLocation one, struct geoLocation two);
+double toRadians(double degree);
+
+void  printClosest(struct latLong*  head, struct latLong  myLocation);
+
+#define MAX_LENGTH 1048576
+int main(int argc, char* argv[]){
+
+    // barefoot solutions
+    struct latLong  myLocation;
+    myLocation.lat = 32.800419;
+    myLocation.lng = -117.234949;
+
+    link temp,node,head;
+    temp = malloc(sizeof(struct latLong));
+    head = temp;
+    node = temp;
+
+    char line[MAX_LENGTH];
+
+    char *latitude;
+    char *longitude;
+
+    FILE* input = fopen(argv[1],"r"); // "r" for read
+
+
+    if(input == NULL){
+        printf("%s","No such file\n");
+        exit(1);
+    } else {
+        while(fgets(line, 1000, input) != NULL){
+
+            printf("Input was: %s \n", line);
+
+            latitude = strtok(line, ",");
+            longitude = strtok(NULL, "");
+
+            printf("token1: %s \n", latitude);
+            printf("token2: %s \n", longitude);
+
+            node->next = malloc(sizeof(*node));
+            node = node->next;
+            node->lat = atof(latitude);
+            node->lng = atof(longitude);
+        }
+
+        fclose(input);
+
+        node->next = NULL;
+        //first node is showing up empty?
+        printAll(head);
+
+        printClosest(head,myLocation);
+
+        //
+    }
+
+/*
+    struct geoLocation point1;
+    struct geoLocation point2;
+
+    // for loop to assign args to geoLocations
+    for (int i = 0; i < argc; i++){
+
+        // DEBUG
+        //printf("command line was %s\n", argv[i]);
+
+        // get points
+        if(i == 0){
+            // skip inital arg
+        } else if(i == 1){
+            sscanf(argv[i],"%lf", &point1.latitude);
+        } else if(i == 2) {
+            sscanf(argv[i],"%lf", &point1.longitude);
+        } else if(i == 3) {
+            sscanf(argv[i],"%lf", &point2.latitude);
+        } else if(i == 4) {
+            sscanf(argv[i],"%lf", &point2.longitude);
+        }
+    }
+
+    // calculate distance
+    double result = distanceBetween(point1,point2);
+
+    // print result
+    printResult(point1,point2,result);
+*/
+    return 0;
+}
+
+
+
+void printAll( link x){
+    //Skips first empty node
+    x=x->next;
+
+    printf("\n");
+    if(x==NULL){
+        return;
+    } else if(x->next==NULL){
+        printf("lat: %f \n", x->lat);
+        printf("lng: %f \n", x->lng);
+        printf("End\n");
+    } else while(x!=NULL){
+        printf("lat: %f \n", x->lat);
+        printf("lng: %f \n", x->lng);
+        printf("\n");
+        x=x->next;
+    }
+    return;
+}
+
+double distanceBetween(struct geoLocation one, struct geoLocation two){
+
+    // convert to radians
+    double lat1 = toRadians(one.latitude);
+    double long1 = toRadians(one.longitude);
+
+    double lat2 = toRadians(two.latitude);
+    double long2 = toRadians(two.longitude);
+
+    // calculate distance points
+    double distance = calculateDistance(lat1,long1,lat2,long2);
+
+    return distance;
+}
+
+double calculateDistance(double lat1, double long1, double lat2, double long2){
+
+    // radious of earth in miles
+    double RadiusMi = 3963.1676;
+
+    // midpoint formula
+    double dlong = (long2 - long1);
+    double dlat = (lat2 - lat1);
+
+    // calculate distance formula
+    double a = sin(dlat/2) * sin(dlat/2) + cos(lat1) * cos(lat2) * sin(dlong/2) * sin(dlong/2);
+    double c = 2 * atan2(sqrt(a), sqrt(1-a));
+
+    // result
+    double result = RadiusMi * c;
+    // convert to yards
+    //result = kilometerToYards(result);
+    return result * 1760;
+}
+
+double toRadians(double degree){
+
+    // multiply coordindate in degrees by pi/180 to get radians
+    // ex:90 degrees
+    // 90 * PI/180 = PI/2 ~ 1.57096
+
+    //DEBUG
+    //printf("%lf in radians is %lf \n", degree, degree * (M_PI/180.00) );
+
+    return degree * (M_PI/180.00);
+}
+
+void printResult(struct geoLocation one, struct geoLocation two, double result){
+    printf("The distance between points (%lf, %lf) and (%lf, %lf) is %.2lf yards. \n", one.latitude, one.longitude, two.latitude, two.longitude, result);
+}
+
+void  printClosest(struct latLong*  head, struct latLong  myLocation){
+    printf("myLoc lat: %f \n", myLocation.lat);
+    printf("myLoc lng: %f \n", myLocation.lng);
+}
+
+
+
+
+
+
