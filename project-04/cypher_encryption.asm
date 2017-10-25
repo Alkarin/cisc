cb38d7662cb57fc6caf7090989858ecbcc49bd0f
AlexanderV <avaughan@barefootsolutions.com>
2017-10-25 16:15:44 -0700

    Initial Message setup

--- project-04/cypher_encryption.asm
+++ project-04/cypher_encryption.asm
@@ -30,20 +30,104 @@
 
 
 # give me 100 of 0x0a
-buffer: .byte 0x0a:100
+#buffer: .byte 0x0a:100
+encrypt_decrypt: .byte 0x0a:4
+addition_key: .byte 0x0a:4
+toggle_key: .byte 0x0a:4
+text_buffer: .byte 0x0a:100
+encryption_result: .byte 0x0a:100
+
 buffer1: .space 100
 
 
 msg : .asciiz "Hello World!" # hardcoded for example
-prompt1: .asciiz "Enter your string \n"
+prompt1: .asciiz "Enter 'e' or 'd' to select encrypt or decrypt:\t"
+prompt2: .asciiz "Enter addition key:\t"
+prompt3: .asciiz "Enter bit toggle key:\t"
+prompt4: .asciiz "Enter text to encrypt:\t"
+prompt5: .asciiz "Encrypted text:\t"
+newline: .asciiz "\n"
 
 ############################################################
 .text
 
+# Prompt the user for encryption or decryption
+	la $a0, prompt1
+	li $v0, 4
+	syscall
+	
+	# Enter user input into memory called "buffer"
+	# RECIEVE INPUT VIA CONSOLE SYSCALL
+	# (Eventually place this in a function)
+
+	# give address of input buffer
+	la $a0, encrypt_decrypt
+	# set size of string length we will read(?) 
+	la $a1, 1
+	# read input string
+	li $v0, 8 
+	syscall
+	
+	la $a0, newline
+	li $v0, 4
+	syscall
+	
+#Prompt the user for addition key
+	la $a0, prompt2
+	li $v0, 4
+	syscall
+	
+	la $a0, addition_key 
+	la $a1, 1
+	li $v0, 8 
+	syscall
+	
+	la $a0, newline
+	li $v0, 4
+	syscall
+	
+#Prompt the user for the bit toggle key
+	
+	la $a0, prompt3
+	li $v0, 4
+	syscall
+	
+	la $a0, toggle_key 
+	la $a1, 1
+	li $v0, 8 
+	syscall
+	
+	la $a0, newline
+	li $v0, 4
+	syscall
+		
+#Prompt the user for the text to encrypt
+	la $a0, prompt4
+	li $v0, 4
+	syscall
+	
+	la $a0, text_buffer
+	la $a1, 20
+	li $v0, 8 
+	syscall
+	
+#Display encrpyed text result
+	la $a0, prompt5
+	li $v0, 4
+	syscall
+	
+	# do a call for the result
+	#encryption_result
+
+	
+	
+	
+##############################################################################################################################################################################
+	
 # Prompt the user for the string
-#la $a0, prompt1
-#li $v0, 4
-#syscall
+	#la $a0, prompt1
+	#li $v0, 4
+	#syscall
 
 
 # Enter user input into memory called "buffer"
@@ -60,8 +144,8 @@ prompt1: .asciiz "Enter your string \n"
 	#syscall
 	
 	
-# FOR HARDCODED msg
-	la 	$t0, msg
+# APPLY ADDITION 
+	la 	$t0, msg	# HARDCODED msg: needs to be user input
 	lb 	$t1, 0 ($t0) 	# load byte # will want in loop to load the next byte, +1 cause byte
 	addi 	$t1, $t1, 4 	# this 4 is coming from user for 'add' so $v0 
 	# add $t1, $t1, $v0
@@ -70,7 +154,7 @@ prompt1: .asciiz "Enter your string \n"
 
 # APPLY TOGGLE
 # again will be from user input
-	li	$s0, 0x01 	# gives value of 0000 0001
+	li	$s0, 0x01 	# HARDCOED 0x01: gives value of 0000 0001
 	li	$v0, 3 		# 3 is user input value, probably wont need this line at all, 
 	sllv	$s0, $s0, $v0 	# instead of $v0, just use input 'toggle' value
 	
